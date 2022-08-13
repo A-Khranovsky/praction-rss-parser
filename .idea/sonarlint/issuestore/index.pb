e193da7b2e236aef447d87b844da33ac9451a762
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-08-13 13:24:09 +0300

    fixed tag parsing

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+S
+#.docker/php-apache/000-default.conf,3\9\39fa5ab4caae8c73bb5e30fbeaf7c1d2fe616b66
+C
+docker-compose.yaml,e\f\ef8def03dc8f162799089e9b5f4321201c8810d5
--- index.php
+++ index.php
@@ -2,23 +2,40 @@
 
 $url = 'https://www.liga.net/news/all/rss.xml';
 
-function objecter()
+function tagParser($source, &$tag, &$params, &$value)
 {
+    $tagBuf = mb_substr($source, mb_strpos($source, '<') + 1, mb_strpos($source, '>') - 1);
+    $buf = explode(' ', $tagBuf);
+    if (count($buf) === 1) {
+        $tag = current($buf);
+    } else {
+        $tag = reset($buf);
+        $params = $buf;
+    }
+    $strTagLen = strlen($tag);
+    $value = mb_substr($source, mb_strpos($source, $tag . '>') + $strTagLen + 1,
+        mb_strpos($source, '</' . $tag) - $strTagLen - 2);
 
 }
 
 function reader($file)
 {
+    $tag = null;
+    $params = null;
+    $value = null;
+
     while (true) {
         $str = fgets($file);
         if (mb_strstr($str, '</item>')) {
             fgets($file);
             break;
         }
-          yield $str;
-//        yield [
-//            mb_substr($str, mb_strpos($str, '<') + 1, mb_strpos($str, '>') - 1) => $str
-//        ];
+
+        tagParser($str, $tag, $params, $value);
+        yield ['key' => $tag,
+            'value' => $value,
+            'params' => $params
+        ];
     }
 }
 
@@ -29,21 +46,29 @@ function parser($url)
     while (!feof($file)) {
         if (fgets($file) == ('<item>' . PHP_EOL)) {
             foreach (reader($file) as $item) {
-                $str [] = $item;
+                $str[$item['key']] = isset($item['params']) ? [$item['value'], $item['params']] : $item['value'];
             }
             yield $str;
             unset($str);
         }
-
     }
-
     fclose($file);
 
 }
 
+$source = [];
+
 $data = parser($url);
 foreach ($data as $item) {
-    echo '<pre>';
-    var_dump($item);
-    echo '</pre>';
+    $obj = new class {
+    };
+    foreach ($item as $key => $value) {
+        $obj->$key = $value;
+    }
+    $source[] = $obj;
+    unset($obj);
 }
+
+echo '<pre>';
+var_dump($source);
+echo '</pre>';
