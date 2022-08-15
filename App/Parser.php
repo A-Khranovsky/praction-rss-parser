2a1255fc49816344b245c9e1d25e1835cd9ef454
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-08-15 11:52:22 +0300

    finished Parser class

--- App/Parser.php
+++ App/Parser.php
@@ -17,7 +17,7 @@ class Parser
         while (!feof($file)) {
             if (fgets($file) == ('<item>' . PHP_EOL)) {
                 $buf = [];
-                foreach (itemHandler($file) as $key => $value) {
+                foreach ($this->itemHandler($file) as $key => $value) {
                     $buf[$key] = $value;
                 }
                 yield new \ArrayObject($buf);
@@ -27,13 +27,35 @@ class Parser
         fclose($file);
     }
 
-    private function itemHandler()
+    private function itemHandler($file)
     {
+        $tag = null;
+        $params = null;
+        $value = null;
 
+        while (true) {
+            $str = fgets($file);
+            if (mb_strstr($str, '</item>')) {
+                fgets($file);
+                break;
+            }
+            $this->tagParser($str, $tag, $params, $value);
+            yield $tag => isset($params) ? ['value' => $value, 'params' => $params] : $value;
+        }
     }
 
-    private function tagParser()
+    private function tagParser($source, &$tag, &$params, &$value)
     {
-
+        $tagBuf = mb_substr($source, mb_strpos($source, '<') + 1, mb_strpos($source, '>') - 1);
+        $buf = explode(' ', $tagBuf);
+        if (count($buf) === 1) {
+            $tag = current($buf);
+        } else {
+            $tag = reset($buf);
+            $params = $buf;
+        }
+        $strTagLen = strlen($tag);
+        $value = mb_substr($source, mb_strpos($source, $tag . '>') + $strTagLen + 1,
+            mb_strpos($source, '</' . $tag) - $strTagLen - 2);
     }
 }
--- index.php
+++ index.php
@@ -1,74 +1,18 @@
 <?php
+spl_autoload_register();
+use App\Parser;
+$url = 'https://www.liga.net/tech/all/rss.xml';
 
-$url = 'https://www.liga.net/news/all/rss.xml';
-
-function tagParser($source, &$tag, &$params, &$value)
-{
-    $tagBuf = mb_substr($source, mb_strpos($source, '<') + 1, mb_strpos($source, '>') - 1);
-    $buf = explode(' ', $tagBuf);
-    if (count($buf) === 1) {
-        $tag = current($buf);
-    } else {
-        $tag = reset($buf);
-        $params = $buf;
-    }
-    $strTagLen = strlen($tag);
-    $value = mb_substr($source, mb_strpos($source, $tag . '>') + $strTagLen + 1,
-        mb_strpos($source, '</' . $tag) - $strTagLen - 2);
-
-}
-
-function reader($file)
-{
-    $tag = null;
-    $params = null;
-    $value = null;
-
-    while (true) {
-        $str = fgets($file);
-        if (mb_strstr($str, '</item>')) {
-            fgets($file);
-            break;
-        }
-
-        tagParser($str, $tag, $params, $value);
-        yield ['key' => $tag,
-            'value' => $value,
-            'params' => $params
-        ];
-    }
-}
-
-function parser($url)
-{
-    $str = [];
-    $file = fopen($url, 'r');
-    while (!feof($file)) {
-        if (fgets($file) == ('<item>' . PHP_EOL)) {
-            foreach (reader($file) as $item) {
-                $str[$item['key']] = isset($item['params']) ? [$item['value'], $item['params']] : $item['value'];
-            }
-            yield $str;
-            unset($str);
-        }
-    }
-    fclose($file);
-
-}
 
 $source = [];
 
-$data = parser($url);
-foreach ($data as $item) {
-    $obj = new class {
-    };
-    foreach ($item as $key => $value) {
-        $obj->$key = $value;
-    }
-    $source[] = $obj;
-    unset($obj);
+$data = new Parser($url);
+foreach ($data->parse() as $item) {
+    $source[] = $item;
 }
 
 echo '<pre>';
 var_dump($source);
 echo '</pre>';
+
+echo memory_get_usage();
